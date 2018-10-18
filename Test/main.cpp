f807a6d91b73ebb80687ce7f10c470d0fc6c65aa
DESKTOP-J8FDL9J\andre <andre.hoeher@gmail.com>
2018-10-18 17:34:51 +0200

    First Upload

---
+++ Test/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+
+
+int main()
+{
+
+	std::string a;
+	
+	std::cout << "Bitte geben sie ihren Vornamen ein: ";
+	std::cin >> a;
+	std::cout << "Hallo " << a << std::endl;
+	
+	return 0;
+}
